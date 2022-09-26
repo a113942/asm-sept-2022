f169d18fb36b7af5a4af2029e5505034f912c3e4
Daniel Sarinana <daniel_e_sarinana@progressive.com>
2022-09-26 17:09:23 -0400

    end of lesson 9-26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asm-sept-2022';
+
+  doMagic(): void{
+    this.title = this.title.toUpperCase();
+  }
+  doMagicAgain(): void{
+    this.title = this.title.toLowerCase();
+  }
 }
