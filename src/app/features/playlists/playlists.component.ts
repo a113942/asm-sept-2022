e1a64caacd33f3389113d9e4ded6f10102345504
Daniel Sarinana <daniel_e_sarinana@progressive.com>
2022-09-28 12:24:39 -0400

    before lunch 09-28

--- src/app/features/playlists/playlists.component.ts
+++ src/app/features/playlists/playlists.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PlaylistFeatureEvents } from './state/actions/feature.actions';
 
 @Component({
   selector: 'app-playlists',
   templateUrl: './playlists.component.html',
   styleUrls: ['./playlists.component.css']
 })
-export class PlaylistsComponent implements OnInit {
+export class PlaylistsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(store:Store) {
+    store.dispatch(PlaylistFeatureEvents.entered());
+   }
 
 }
